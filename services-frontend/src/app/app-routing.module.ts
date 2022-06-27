46a52e425c366d78d32c3e74c79038cca9942419
gamabarbara <barbara.cp@outlook.com>
2022-06-26 21:13:31 -0300

    Atualizations of Angular Projects and exercises

---
+++ services-frontend/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'funcionarios', 
+    loadChildren: () => import('./funcionarios/funcionarios.module').then(m => m.FuncionariosModule)
+    //Lazy loading
+  }, 
+  {
+    path: '', 
+    pathMatch: 'full', 
+    redirectTo: 'funcionarios'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ services-frontend/src/app/funcionarios/pages/listar-funcionarios/listar-funcionarios.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Funcionario } from '../../models/funcionario';
+import { FuncionarioService } from '../../services/funcionario.service';
+
+@Component({
+  selector: 'app-listar-funcionarios',
+  templateUrl: './listar-funcionarios.component.html',
+  styleUrls: ['./listar-funcionarios.component.css']
+})
+export class ListarFuncionariosComponent implements OnInit {
+
+  funcionarios: Funcionario[] = []
+  colunas: Array<string> = ['id', 'nome', 'email', 'actions']
+
+  constructor(
+    private funcService: FuncionarioService
+  ) { }
+
+  ngOnInit(): void {
+    // 1° sucesso -> retorna os dados
+    // 2° erro -> ocorre um erro na fonte de dados
+    // 3° complete -> a fonte de dados te retorna tudo
+
+    this.funcService.getFuncionarios().subscribe(
+      (funcs) => { // sucesso
+        this.funcionarios = funcs
+      },
+      (erro) => { // erro
+        console.log(erro)
+      },
+      () => { // complete
+        console.log('Dados enviados com sucesso')
+      }
+    )
+  }
+}
