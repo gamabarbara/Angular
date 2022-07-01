ce5c0755cf885639080dcec6c1a6c5071c0c3840
gamabarbara <barbara.cp@outlook.com>
2022-07-01 11:06:42 -0300

    Moments project mande in Angular

--- moments/src/app/components/moment-form/moment-form.component.ts
+++ moments/src/app/components/moment-form/moment-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Moment } from '../Moments';
 
 @Component({
   selector: 'app-moment-form',
@@ -6,10 +8,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./moment-form.component.css']
 })
 export class MomentFormComponent implements OnInit {
+  
+  @Output() onSubmit = new EventEmitter<Moment>();
+  @Input() btnText!: string
+  @Input() momentData: Moment | null = null;
+  momentForm!: FormGroup;
+
+  
 
   constructor() { }
 
   ngOnInit(): void {
+    this.momentForm = new FormGroup({
+      id: new FormControl(this.momentData ? this.momentData.id: ''),
+      title: new FormControl(this.momentData ? this.momentData.title: '', [Validators.required]),
+      description: new FormControl(this.momentData ? this.momentData.description: '', [Validators.required]),
+      image: new FormControl(''),
+    });
+  }
+
+  get title(){
+    return this.momentForm.get('title')!;
+  }
+  get description(){
+    return this.momentForm.get('description')!;
+  }
+
+  onFileSelected(event: any){
+    const file: File = event.target.files[0]
+    this.momentForm.patchValue({image: file})
+  }
+
+  submit(){
+    if(this.momentForm.invalid){
+      return;
+    }
+    console.log(this.momentForm.value)
+    this.onSubmit.emit(this.momentForm.value)
   }
 
 }
--- moments/src/app/components/pages/new-moment/new-moment.component.ts
+++ moments/src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { MomentService } from 'src/app/services/moment.service';
+import { Moment } from '../../Moments';
+import { MessagesService } from 'src/app/services/messages.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-moment',
   templateUrl: './new-moment.component.html',
-  styleUrls: ['./new-moment.component.css']
+  styleUrls: ['./new-moment.component.css'], 
 })
 export class NewMomentComponent implements OnInit {
-
-  constructor() { }
+  btnText = "Compartilhar!";
+  
+  constructor(
+    private momentService: MomentService, 
+    private messagesService: MessagesService, 
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
   }
 
+  async createHandler(moment: Moment){
+    const formData = new FormData()
+    formData.append("title", moment.title)
+    formData.append("description", moment.description)
+
+    if(moment.image){
+      formData.append('image', moment.image)
+    }
+    
+
+    await this.momentService.createMoment(formData).subscribe();
+    this.messagesService.add('Momento adicionado com sucesso')
+    this.router.navigate(['/']);
+
+    // 1° Enviar para o service
+    // 2° Exibir mensagem
+    // 3° Redirect
+  }
 }
